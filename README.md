0897c7c70fc9a03af78916e223c3c65ead63344b
iluvpidgeons <gamernoahaza@gmail.com>
2025-10-22 14:27:19 -0400

    updated random.sh and adds a readme.md

---
+++ README.md
@@ -0,0 +1,4 @@
+lowkey I barely know how to use github yet so this is where I save my scripts
+
+quick-launch.sh give you the option to launch either firefox or steam
+random.sh asks you if you're a diddyblud mango and if you're not it shuts down in 5 seconds
--- random.sh
+++ random.sh
@@ -4,5 +4,17 @@ read -p "are you a diddyblud (y/n): " answer
 if [[ "$answer" =~ ^[Yy]$ ]]; then
     echo "mango six seven"
 else
-    echo "lowkey I would shutdown or something but I dont wanna do that"
+    echo "im finna shut ur shit down in 5 seconds"
+    echo "5"
+    sleep 1
+    echo "4"
+    sleep 1
+    echo "3"
+    sleep 1
+    echo "2"
+    sleep 1
+    echo "1"
+    sleep 1
+    echo "ok bye"
+    shutdown
 fi
